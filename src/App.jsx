fbf871fc5e91d2a12ff4043bea48c85195b10dd6
Trung Nguyen <trung150502@gmail.com>
2023-11-19 17:35:32 +0700

    Update quotes

--- src/App.jsx
+++ src/App.jsx
@@ -27,12 +27,13 @@ const App = () => {
 
   const showLogin = !user;
 
+  // https://www.reddit.com/r/ProgrammerHumor/comments/108auwz/guys_i_want_to_put_a_funny_programming_quote_on/
   window.sessionStorage.setItem("allMessages", JSON.stringify({
-    "0": [{"from":"4","date":1697093948,"message":"JCAPOSKC*(8)","roomId":"0"},{"from":"1","date":1697094148,"message":"1 + 1 = 3","roomId":"0"},{"from":"1","date":1697094348,"message":"Chao ban??","roomId":"0"},{"from":"3","date":1697094548,"message":"Tam biet","roomId":"0"},{"from":"4","date":1700123793,"message":"dasdsa","roomId":"0"},{"from":"1","date":1700127946,"message":"jieuoiufo;sjdo","roomId":"0"},{"from":"1","date":1700127951,"message":"isld a dsjadlkjsada dlisajdl","roomId":"0"},{"from":"1","date":1700127958,"message":"jslks;dsada","roomId":"0"}],
-    "1:4": [{"from":"4","date":1697094704,"message":"Day La mot tin nhan","roomId":"1:4"},{"from":"1","date":1697094746,"message":"($)@!)#)_+)+!3","roomId":"1:4"}],
-    "1:2": [{"from":"2","date":1700126273,"message":"jsladjlsad","roomId":"1:2"},{"from":"2","date":1700126276,"message":"akdlskf;sa\\nkd;lsa","roomId":"1:2"}],
-    "2:3": [{"from":"3","date":1697094544,"message":"Day La mot tin nhan voi emoji 😎","roomId":"2:3"},{"from":"2","date":1697094585,"message":"Hello Friend 🏄🏻🏄🏻🏄🏻","roomId":"2:3"}],
-    "2:4": [{"from":"4","date":1697094685,"message":"Day La mot tin nhan voi ki tu dac biet !2#!$2","roomId":"2:4"},{"from":"2","date":1697094738,"message":"Day La mot tin nhan","roomId":"2:4"}]
+    "0": [{"from":"4","date":1697093948,"message":"boss make a dollar; I make a dime; that's why my algorithms run in exponential time","roomId":"0"},{"from":"1","date":1697094148,"message":"There are 10 types of people: those who understand binary and those who don’t","roomId":"0"},{"from":"1","date":1697094348,"message":"","roomId":"0"},{"from":"3","date":1697094548,"message":"","roomId":"0"},{"from":"4","date":1700123793,"message":"There are two ways to write error-free programs; only the third one works.🍾","roomId":"1:4"},{"from":"1","date":1697094746,"message":"A user interface is like a joke. If you have to explain it, it’s not that good 🍸","roomId":"0"},{"from":"1","date":1700127946,"message":"jieuoiufo;sjdo","roomId":"0"},{"from":"1","date":1700127951,"message":"Before software can be reusable it first has to be usable🥑","roomId":"0"},{"from":"1","date":1700127958,"message":"If one programmer can do it in one week, then 2 programmers can do it in 2 weeks!","roomId":"0"}],
+    "1:4": [{"from":"4","date":1697094704,"message":"There are two ways to write error-free programs; only the third one works.🍾","roomId":"1:4"},{"from":"1","date":1697094746,"message":"A user interface is like a joke. If you have to explain it, it’s not that good 🍸","roomId":"1:4"}],
+    "1:2": [{"from":"2","date":1700126273,"message":"Programming is the art of adding bugs to an empty text file.","roomId":"1:2"},{"from":"2","date":1700126276,"message":"akdlskf;sa\\nkd;lsa","roomId":"1:2"}],
+    "2:3": [{"from":"3","date":1697094544,"message":"I told him I can't open the JAR, and he said go download Java ☕️","roomId":"2:3"},{"from":"2","date":1697094585,"message":"In order to understand recursion, one must first understand recursion 😎","roomId":"2:3"}],
+    "2:4": [{"from":"4","date":1697094685,"message":"A good programmer is someone who always looks both ways before crossing a one-way street.🥝","roomId":"2:4"},{"from":"2","date":1697094738,"message":"The trouble is that you can never tell what a programmer is doing until it’s too late.🥗","roomId":"2:4"}]
   }));
 
   return (
